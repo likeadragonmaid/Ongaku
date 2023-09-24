a331e5eec703e597966e8107779e4bf291774860
anonymousx97 <88324835+anonymousx97@users.noreply.github.com>
2023-09-24 10:41:06 +0530

    launch.sh: Fix wakelock logic.

--- launch.sh
+++ launch.sh
@@ -4,7 +4,7 @@ echo "Ongaku: Checking for updates"
 git fetch && git pull
 source venv/bin/activate
 
-if [ $termux -eq 1 ]; then
+if [ $(echo $HOME | grep termux) ]; then
 	echo -e "Termux: Acquiring wakelock"
 	termux-wake-lock
 fi
@@ -12,7 +12,7 @@ fi
 python3 -m ongaku
 deactivate
 
-if [ $termux -eq 1 ]; then
+if [ $(echo $HOME | grep termux) ]; then
 	echo -e "Termux: Releasing wakelock"
 	termux-wake-unlock
 fi
